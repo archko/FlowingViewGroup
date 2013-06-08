703daef4dfa771e06786a0b49f12085d1d22684d
archko <archko@sina.com>
2013-06-08 17:49:15 +0800

    修改流布局,使其可以每行居中布局元素,避免重叠. 支持RelativeLayout作为ListView的item. 修改示例.

--- src/com/eyebrowssoftware/example/fvg/FlowingViewGroup.java
+++ src/com/eyebrowssoftware/example/fvg/FlowingViewGroup.java
@@ -3,18 +3,25 @@ package com.eyebrowssoftware.example.fvg;
 import java.util.LinkedList;
 
 import android.app.ListActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
+/**
+ * @author archko@gmail.com
+ */
 public class FlowingViewGroup extends ListActivity {
 
-	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.flowing);
         
         TagsViewGroup tvg = (TagsViewGroup) findViewById(R.id.tags);
         ArrayAdapter<String> saa = new ArrayAdapter<String>(this, R.layout.text_view, mTitles);
@@ -24,24 +31,28 @@ public class FlowingViewGroup extends ListActivity {
         for(int i = 0; i < mTitles.length; ++i) {
         	LinkedList<String> sl = new LinkedList<String>();
         	for(int j = i; j < mTitles.length; ++j) {
-        		sl.add(mTitles[j]);
+                sl.add(mTitles[j]);          break; //test one line
         	}
         	String[] empty = {};
-        	mLibraries.add(sl.toArray(empty));
+            mLibraries.add(sl.toArray(empty));       break; //test one line
         }
-       ArrayAdapter<String[]> saaa = new ArrayAdapter<String[]>(this, R.layout.tags_view_group, mLibraries) {
+
+       ArrayAdapter<String[]> saaa = new ArrayAdapter<String[]>(this, R.layout.tags_view_group2, mLibraries) {
         	
         	@Override
         	public View getView(int position, View convertView, ViewGroup parent) {
-        		TagsViewGroup tgv;
+                RelativeLayout layout;
+                TagsViewGroup tgv=null;
         		if(convertView == null) {
-        			tgv = (TagsViewGroup) FlowingViewGroup.this.getLayoutInflater().inflate(R.layout.tags_view_group, null);
-        		} else
-        			tgv = (TagsViewGroup) convertView;
-        		ArrayAdapter<String> aa = new ArrayAdapter<String>(FlowingViewGroup.this, 
-        				R.layout.text_view, this.getItem(position));
-        		tgv.setAdapter(aa);
-        		return tgv;
+                    layout=(RelativeLayout) FlowingViewGroup.this.getLayoutInflater().inflate(R.layout.tags_view_group2, null);
+                    tgv=(TagsViewGroup) layout.findViewById(R.id.tag);
+                } else {
+                    layout=(RelativeLayout) convertView;
+                    tgv=(TagsViewGroup) layout.findViewById(R.id.tag);
+                }
+
+                tgv.setAdapter(new ImageAdapter(FlowingViewGroup.this));
+                return layout;
         	}
         };
         this.setListAdapter(saaa);
@@ -63,5 +74,92 @@ public class FlowingViewGroup extends ListActivity {
         "Jack of Fables",
         "Strangers in Paradise",
     };
-    
+
+    public class ImageAdapter extends BaseAdapter {
+
+        private static final int ITEM_WIDTH=136;
+        private static final int ITEM_HEIGHT=88;
+
+        //private final int mGalleryItemBackground;
+        private final Context mContext;
+
+        private final Integer[] mImageIds={
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.image_loading,
+            R.drawable.image_loading,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.image_loading,
+            R.drawable.image_loading,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.icon,
+            R.drawable.image_loading,
+            R.drawable.image_loading,
+        };
+
+        private final float mDensity;
+        private final LayoutInflater mInflater;
+
+        public ImageAdapter(Context c) {
+            mContext=c;
+            mDensity=c.getResources().getDisplayMetrics().density;
+            mInflater=LayoutInflater.from(c);
+        }
+
+        public int getCount() {
+            return mImageIds.length;
+        }
+
+        public Object getItem(int position) {
+            return position;
+        }
+
+        public long getItemId(int position) {
+            return position;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // A ViewHolder keeps references to children views to avoid unneccessary calls
+            // to findViewById() on each row.
+            ViewHolder holder;
+
+            // When convertView is not null, we can reuse it directly, there is no need
+            // to reinflate it. We only inflate a new View when the convertView supplied
+            // by ListView is null.
+            if (convertView==null) {
+                convertView=mInflater.inflate(R.layout.home_time_line_item_img, null);
+
+                // Creates a ViewHolder and store references to the two children views
+                // we want to bind data to.
+                holder=new ViewHolder();
+                holder.picture=(ImageView) convertView.findViewById(R.id.picture);
+                //holder.pictureLay=(ImageView) convertView.findViewById(R.id.status_picture_lay);
+
+                convertView.setTag(holder);
+            } else {
+                // Get the ViewHolder back to get fast access to the TextView
+                // and the ImageView.
+                holder=(ViewHolder) convertView.getTag();
+            }
+
+            holder.picture.setImageResource(mImageIds[position]);
+            return convertView;
+        }
+    }
+
+    static class ViewHolder {
+
+        ImageView picture;
+        ImageView pictureLay;
+    }
+
 }
--- src/com/eyebrowssoftware/example/fvg/TagsViewGroup.java
+++ src/com/eyebrowssoftware/example/fvg/TagsViewGroup.java
@@ -7,20 +7,23 @@ import java.util.Stack;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.database.DataSetObserver;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.util.SparseIntArray;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListAdapter;
 
 /**
  * @author Brion Emde
- *
+ * @author archko@gmail.com
  */
 public class TagsViewGroup extends ViewGroup {
 	@SuppressWarnings("unused")
 	private static final String TAG = "TagsAdapterView";
 	
-	private static final int MIN_ROWS = 2;
+	private static final int MIN_ROWS = 1;
 	
 	private ListAdapter mAdapter;
 	private int mHorizontalSpacing = 0;
@@ -31,10 +34,22 @@ public class TagsViewGroup extends ViewGroup {
 	private int mPaddingLeft;
 	private int mPaddingRight;
 	private int mMinRows;
+    /**
+     * after measure,we get the rows
+     */
+    private int mRows;
+    /**
+     * every row height
+     */
+    private SparseIntArray mRowsHeight;
 
-	
-	private ViewGroup.MarginLayoutParams params = 
-		new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT); 
+    /**
+     * Should be used by subclasses to listen to changes in the dataset
+     */
+    AdapterDataSetObserver mDataSetObserver;
+
+	private MarginLayoutParams params =
+		new MarginLayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 
 	public TagsViewGroup(Context context) {
 		super(context);
@@ -110,32 +125,31 @@ public class TagsViewGroup extends ViewGroup {
 	
 	private Stack<View> tvs = new Stack<View>();
 
-	public void setAdapter(ListAdapter adapter) {
-		if((mAdapter = adapter) == null) return;
-		
-		// Cache any pre-existing children, from being recycled.
-		int ccount = this.getChildCount(); 
-		for(int i = 0; i < ccount; i++) {
-			tvs.push(this.getChildAt(i));
-		}
-		removeAllViews();
-		int items = mAdapter.getCount();
-		// Log.d(TAG, "Adapter count: " + String.valueOf(items));
-		for(int i = 0; i < items; ++i) {
-			View tv = mAdapter.getView(i, (tvs.empty()) ? null : tvs.pop(), this);
-			if(mViewHeight < 0) {
-				tv.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
-				mViewHeight = tv.getMeasuredHeight();
-			}
-			this.addView(tv, i, params);
-		}
-		requestLayout();
-	}
+    public void setAdapter(ListAdapter adapter) {
+        //if((mAdapter = adapter) == null) return;
+        if (mAdapter != null && mDataSetObserver != null) {
+            mAdapter.unregisterDataSetObserver(mDataSetObserver);
+            mDataSetObserver=null;
+        } else {
+            //System.out.println("adapter == null remove all view.");
+            removeAllViews();
+            tvs.clear();
+            requestLayout();
+        }
+
+        if (null!=adapter) {
+            mAdapter=adapter;
+            mDataSetObserver=new AdapterDataSetObserver();
+            mAdapter.registerDataSetObserver(mDataSetObserver);
+
+            handleDataChanged();
+        }
+    }
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 
-		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+		/*int widthMode = MeasureSpec.getMode(widthMeasureSpec);
 		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
 		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
 		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
@@ -233,9 +247,136 @@ public class TagsViewGroup extends ViewGroup {
 		if(measuredWidth < suggestedMinWidth)
 			measuredWidth = suggestedMinWidth;
 		setMeasuredDimension(measuredWidth, measuredHeight);
-		// Log.d(TAG, "Measurement: width = " + String.valueOf(measuredWidth) + " height: " + String.valueOf(measuredHeight));
+		// Log.d(TAG, "Measurement: width = " + String.valueOf(measuredWidth) + " height: " + String.valueOf(measuredHeight));*/
+        doMeasure(widthMeasureSpec, heightMeasureSpec);
 	}
 	
+    private void doMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        // Log.d(TAG, "-----------------------------------------");
+        // Log.d(TAG, "Measure Dimensions: widthSize = " + String.valueOf(widthSize)
+        //		+ " heightSize: " + String.valueOf(heightSize));
+
+
+        mPaddingTop = getPaddingTop();
+        mPaddingBottom = getPaddingBottom();
+        mPaddingLeft = getPaddingLeft();
+        mPaddingRight = getPaddingRight();
+
+        // Log.d(TAG, "Measure Padding: Top = " + String.valueOf(mPaddingTop) + "Bottom: " + String.valueOf(mPaddingBottom)
+        // 		+ " Left = " + String.valueOf(mPaddingLeft) + " Right = " + String.valueOf(mPaddingRight));
+
+        int measuredHeight = 0;
+        int measuredWidth= 0;
+        int children_height = 0;
+        int children_width = 0;
+
+        int suggestedMinWidth = this.getSuggestedMinimumWidth();
+        int suggestedMinHeight = this.getSuggestedMinimumHeight();
+
+        @SuppressWarnings("unused")
+        int height = 0;
+        int width = 0;
+
+        if(widthMode == MeasureSpec.UNSPECIFIED) {
+            // Log.d(TAG, "Unspecified");
+            // we'll have a single row of tags, respecing the padding.
+            int ccount = this.getChildCount();
+            for(int i = 0; i < ccount; ++i) {
+                View tv = this.getChildAt(i);
+                tv.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED);
+                height = tv.getMeasuredHeight();
+                width = tv.getMeasuredWidth();
+                // Log.d(TAG, "Unspecified Simple: width = " + String.valueOf(width) + " height: " + String.valueOf(height));
+                children_width += tv.getMeasuredWidth();
+                if((i+1) < ccount) {
+                    children_width += mHorizontalSpacing;
+                }
+                measuredHeight=Math.max(measuredHeight, height);
+            }
+            // Log.d(TAG, "-----------------------------------------");
+            measuredHeight = mPaddingTop + measuredHeight + mPaddingBottom+ mVerticalSpacing;
+            measuredWidth = children_width + mPaddingLeft + mPaddingRight;
+        } else {	// MeasureSpec.AT_MOST or MeasureSpec.EXACTLY
+            if (null==mRowsHeight) {
+                mRowsHeight=new SparseIntArray();
+            } else {
+                mRowsHeight.clear();
+            }
+            // Log.d(TAG, "Other");
+            measuredWidth = 0; // use the max allowed
+            children_width = widthSize - mPaddingLeft - mPaddingRight;
+            int pos = 0;
+            int row = 0;
+            int ccount = this.getChildCount();
+            int allMeasureHeight=0;
+            int lineMaxHeight=0;
+            for(int i = 0; i < ccount; ++i) {
+                View tv = this.getChildAt(i);
+                tv.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED);
+                height = tv.getMeasuredHeight();
+                width = tv.getMeasuredWidth();
+                // Log.d(TAG, "Other Simple: width = " + String.valueOf(width) + " height: " + String.valueOf(height));
+                if(pos + width + mHorizontalSpacing > children_width) {
+                    //Log.d(TAG, "Starting new row, next_post = " + String.valueOf(pos)+" row:"+row);
+                    pos = 0;
+                    allMeasureHeight+=lineMaxHeight+mVerticalSpacing;
+                    mRowsHeight.put(row,lineMaxHeight);
+                    ++row;
+                }
+
+                pos+=width+mHorizontalSpacing;
+                measuredWidth=Math.max(pos, measuredWidth); //这样可以是wrap_content,否则是充满宽
+                lineMaxHeight=Math.max(height, lineMaxHeight);
+                /*Log.d(TAG, "measure =row:"+row+" pos:"+pos+" width:"+width+" maxWidth:"+measuredWidth+
+                    " lineMaxHeight:"+lineMaxHeight+" height:"+height+" allH:"+allMeasureHeight);*/
+                // Log.d(TAG, "Extending row, next_post = " + String.valueOf(pos));
+            }
+
+            mRowsHeight.put(row, lineMaxHeight);
+
+            measuredWidth+=mPaddingLeft+mPaddingRight;
+            allMeasureHeight+=lineMaxHeight;    //add last line height
+            measuredHeight=allMeasureHeight;
+            // Log.d(TAG, "-----------------------------------------");
+
+            //int rows_written=row<=1 ? row : row+1;
+            switch(heightMode){
+                case MeasureSpec.AT_MOST:
+                    //Log.d(TAG, "heightMode: AT_MOST");
+                    children_height =  (measuredHeight ) ;
+                    measuredHeight = children_height + mPaddingTop + mPaddingBottom;
+                    measuredHeight = (measuredHeight > heightSize) ? heightSize : measuredHeight;
+                    break;
+                case MeasureSpec.EXACTLY:
+                    //Log.d(TAG, "heightMode: EXACTLY");
+                    measuredHeight = heightSize;
+                    break;
+                case MeasureSpec.UNSPECIFIED:
+                    //Log.d(TAG, "heightMode: UNSPECIFIED");
+                    /*if(rows_written < mMinRows)
+                        rows_written = mMinRows;*/
+                    children_height =  (measuredHeight ) ;
+                    measuredHeight = children_height + mPaddingTop + mPaddingBottom;
+                    break;
+                default:
+                    break;
+            }
+            /*Log.d(TAG, "rows_written:"+" mViewHeight:"+allMeasureHeight+" heightSize:"+heightSize
+                +" children_height:"+children_height);*/
+        }
+        if(measuredHeight < suggestedMinHeight)
+            measuredHeight = suggestedMinHeight;
+        if(measuredWidth < suggestedMinWidth)
+            measuredWidth = suggestedMinWidth;
+        setMeasuredDimension(measuredWidth, measuredHeight);
+        //Log.d(TAG, "Measurement: width = "+String.valueOf(measuredWidth)+" height: "+String.valueOf(measuredHeight));
+    }
+
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
 
@@ -247,20 +388,70 @@ public class TagsViewGroup extends ViewGroup {
 		
 		int pos = mPaddingLeft;
 		int row = 0;
-		r -= mPaddingRight;
+		//r -= mPaddingRight;
+        r=getWidth()-mPaddingRight;
 		for(int i = 0; i < this.getChildCount(); ++i) {
 			View tv = this.getChildAt(i);
+            if (tv.getVisibility()==GONE){
+                continue;
+            }
 			int width = tv.getMeasuredWidth();
 			int height = tv.getMeasuredHeight();
-			if((pos + width + mHorizontalSpacing) >= r) {
+			if((pos + width + mHorizontalSpacing) > r) {
 				pos = mPaddingLeft;
 				++row; 
 			}
-			int child_top = mPaddingTop + row * (mViewHeight + mVerticalSpacing);
+            /*Log.d(TAG, "layout row:"+row+" pos:"+pos+" width:"+width+" mHSpacing:"+
+                mHorizontalSpacing+" r:"+r+" mw:"+getWidth()+" mVSpacing:"+ mVerticalSpacing+" height:"+height+" allh:"+getHeight());*/
+
+            int rowHeight=mRowsHeight.get(row);
+            int allHeight=0;
+            for(int j=0;j<row;j++){
+                allHeight+=mRowsHeight.get(j);
+            }
+			//int child_top = mPaddingTop + row * (height + mVerticalSpacing);
+            int child_top=(rowHeight-height)/2+allHeight+mPaddingTop+mVerticalSpacing*row;
 			tv.layout(pos, child_top, pos + width, child_top + height);
 			// Log.d(TAG, "child: Top = " + String.valueOf(child_top) + "Bottom: " + String.valueOf(child_top+height) 
 			//		+ " Left = " + String.valueOf(pos) + " Right = " + String.valueOf(pos + width));
 			pos += width + mHorizontalSpacing;
 		}
+	}
+
+    class AdapterDataSetObserver extends DataSetObserver {
+        @Override
+        public void onChanged() {
+            super.onChanged();
+            handleDataChanged();
+        }
+
+        @Override
+        public void onInvalidated() {
+            super.onInvalidated();
+            handleDataChanged();
+        }
+    }
+
+    private void handleDataChanged() {
+        //System.out.println("handleDataChanged.");
+        removeAllViews();
+        tvs.clear();
+
+        // Cache any pre-existing children, from being recycled.
+        int ccount = this.getChildCount();
+        for(int i = 0; i < ccount; i++) {
+            tvs.push(this.getChildAt(i));
+        }
+        int items = mAdapter.getCount();
+        // Log.d(TAG, "Adapter count: " + String.valueOf(items));
+        for(int i = 0; i < items; ++i) {
+            View tv = mAdapter.getView(i, (tvs.empty()) ? null : tvs.pop(), this);
+            /*if(mViewHeight < 0) {   //RelativeLayout会有问题,而且这种方式只计算第一个子元素的高,然后用这个高来处理所有的.
+                tv.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED);
+                mViewHeight = tv.getMeasuredHeight();
+            }*/
+            this.addView(tv, i, params);
+        }
+        requestLayout();
 	}
 }
